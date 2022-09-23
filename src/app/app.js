ed52a2141b36850018e4f7061833ce664a121199
Kent <dylankentish@live.co.uk>
2022-09-23 23:44:07 +0100

    default app

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app'
 
 // Use your config values here.
-const app = initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-});
+const app = initializeApp();
 
 export {
     app
